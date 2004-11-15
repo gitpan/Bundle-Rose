047879d47dde3503808b1a23b0b31a956e58e054
John Siracusa <siracusa@gmail.com>
2004-11-15 15:11:44 +0000

    Import of JSIRACUSA/Bundle-Rose-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-Rose
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JSIRACUSA/Bundle-Rose-0.01.tar.gz
    gitpan-cpan-author:       JSIRACUSA
    gitpan-cpan-maturity:     released

---
+++ lib/Bundle/Rose.pm
@@ -0,0 +1,39 @@
+package Bundle::Rose;
+
+use strict;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::Rose - Bundle all modules in thee Rose web application construction suite.
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::Rose'>
+
+=CONTENTS
+
+Rose
+
+Rose::DateTime
+
+Rose::HTML::Objects
+
+Rose::Object
+
+Rose::URI
+
+=head1 AUTHOR
+
+John C. Siracusa (siracusa@mindspring.com)
+
+=head1 COPYRIGHT
+
+Copyright (c) 2004 by John C. Siracusa.  All rights reserved.  This program is
+free software; you can redistribute it and/or modify it under the same terms
+as Perl itself.
